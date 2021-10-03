71b0185f30ff54f75e47523717478c09ce9cbd28
Angelo Dias <oiangelodias@gmail.com>
2021-10-03 15:22:44 -0300

    Add Edit possibility

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -20,4 +24,15 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated succesfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end

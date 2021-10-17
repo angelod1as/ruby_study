b8168e6f240871c6951aa057f9f7eb031290538c
Angelo Dias <oiangelodias@gmail.com>
2021-10-17 16:20:08 -0300

    downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username,
             presence: true,
